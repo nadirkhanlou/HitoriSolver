4f1a3c101a66ce4f5f555b0e8de95b98d7832b5f
nadirkhanlou <39118903+nadirkhanlou@users.noreply.github.com>
2019-12-02 20:48:47 +0330

    Updated HitoriSolverCore

--- HitoriCore/HitoriSolverCore.cpp
+++ HitoriCore/HitoriSolverCore.cpp
@@ -1,6 +1,6 @@
+#include "HitoriSolverCore.h"
 #include <algorithm>
 #include <queue>
-#include "HitoriSolverCore.h"
 
 namespace HitoriSolverCore {
 State::State() {}
@@ -10,7 +10,7 @@ State::~State() {}
 bool State::IsGoal() { return true; }
 
 std::vector<State>* State::Successor(SearchType searchType,
-                                     float (*heuristic)(State)) {
+                                     double (*heuristic)(State)) {
   return {};
 }
 
@@ -18,13 +18,13 @@ HitoriSolver::HitoriSolver(const char* filePath) {}
 
 HitoriSolver::~HitoriSolver() {}
 
-State HitoriSolver::GreedyBfs(State initialState, float (*heuristic)(State)) {
+State HitoriSolver::GreedyBfs(State initialState, double (*heuristic)(State)) {
   // Create a new minimum priority queue (min heap) and insert the initial state
   // into it. States with compared to each other based on their score =
   // heuristic. States with lower scores are higher up in the heap.
   std::priority_queue<
       std::priority_queue<int, std::vector<State>, std::greater<State>>,
-      std::vector<State>, std::greater<State>>
+      std::vector<State>, StateGreaterComparator>
       priorityQueue;
   priorityQueue.push(initialState);
 
@@ -57,11 +57,11 @@ State HitoriSolver::GreedyBfs(State initialState, float (*heuristic)(State)) {
   return State();
 }
 
-State HitoriSolver::AStar(State initialState, float (*heuristic)(State)) {
+State HitoriSolver::AStar(State initialState, double (*heuristic)(State)) {
   // Create a new minimum priority queue (min heap) and insert the initial state
   // into it. States with compared to each other based on their score = (cost so
   // far + heuristic). States with lower scores are higher up in the heap.
-  std::priority_queue<State, std::vector<State>, std::greater<State>>
+  std::priority_queue<State, std::vector<State>, StateGreaterComparator>
       priorityQueue;
   priorityQueue.push(initialState);
 
@@ -95,7 +95,7 @@ State HitoriSolver::AStar(State initialState, float (*heuristic)(State)) {
 }
 
 State HitoriSolver::HillClimbing(State initialState,
-                                 float (*heuristic)(State)) {
+                                 double (*heuristic)(State)) {
   // Initialize a currentState variable with the initial state
   State currentState = initialState;
   while (true) {
@@ -110,7 +110,7 @@ State HitoriSolver::HillClimbing(State initialState,
 
     // Find the state with the score (heuristic)
     auto bestSuccessor = std::min_element(
-        successors->begin(), successors->end(), stateLessCompartor);
+        successors->begin(), successors->end(), StateLessComparator);
 
     // If there was a successor with a better score than the current tate,
     // continue the search with that successor. Otherwise
@@ -125,4 +125,59 @@ State HitoriSolver::HillClimbing(State initialState,
   // minimum)
   return currentState;
 }
+
+State HitoriSolver::HillClimbing(State initialState,
+                                 double (*heuristic)(State)) {
+  // Initialize a currentState variable with the initial state
+  State currentState = initialState;
+  while (true) {
+    // Check whether the current state is a goal
+    if (currentState.IsGoal()) {
+      return currentState;
+    }
+
+    // Generate the successors of the current state
+    std::vector<State>* successors =
+        currentState.Successor(SearchType::HillClimbing, heuristic);
+
+    // Now each state will be assigned a probability according to its score
+    // (heuristic). The higher the score is, the lower the probability will be.
+    // This is done using a softmax function
+    auto softmax =
+        [](std::vector<double>& scores) {
+          double sum = 0.f;
+          for (auto it = scores.begin(); it != scores.end(); ++it) {
+            // Since we want higher scores to have less probability, we use
+            // exp(-x) instead of exp(x - MaximumScore).
+            *it = std::exp(-*it);
+            sum += *it;
+          }
+          for (auto it = scores.begin(); it != scores.end(); ++it) {
+            *it /= sum;
+          }
+        }
+
+    std::vector<double> probabilities;
+    double sum;
+    for (auto it = successors->begin(); successors->end(); ++it) {
+      probabilities.push_back(worstSuccessor->score - it->score)
+    }
+    // Apply softmax to probabilities (which is initially a vector of numbers,
+    // NOT actual probabilities)
+    softmax(probabilities);
+
+    // If there was a successor with a better score than the current tate,
+    // continue the search with that successor. Otherwise
+    if (bestSuccessor->score <= currentState.score)
+      currentState = *bestSuccessor;
+    else {
+      break;
+    }
+  }
+
+  // Return the current state (which is a local minimum and NOT a global
+  // minimum)
+  return currentState;
+}
+
 }  // namespace HitoriSolverCore
--- HitoriCore/HitoriSolverCore.h
+++ HitoriCore/HitoriSolverCore.h
@@ -1,32 +1,40 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <math>
+#include <random>
 
 namespace HitoriSolverCore {
 
 enum class SearchType { GreedyBfs, AStar, HillClimbing, SimulatedAnnealing };
 
 // Custom less and greater operators for  State scores. Used in priority queues.
-auto stateLessCompartor = [](State lhs, State rhs) {
-  return lhs.score < rhs.score;
+class StateLessComparator {
+ public:
+  bool operator()(const State& lhs, const State& rhs) {
+    return lhs.score < rhs.score;
+  }
 };
-auto stateGreaterCompartor = [](State lhs, State rhs) {
-  return lhs.score < rhs.score;
+class StateGreaterComparator {
+ public:
+  bool operator()(const State& lhs, const State& rhs) {
+    return lhs.score > rhs.score;
+  }
 };
 
 class State {
  public:
-  float score;  // The score based on which the state will be placed in the
+  double score;  // The score based on which the state will be placed in the
                 // priority queue. How the score is calculated is dependent on
                 // the search algorithm (searchType).
-  float costSoFar;  // The actual cost to reach this state.
+  double costSoFar;  // The actual cost to reach this state.
 
   State();
   ~State();
 
   bool IsGoal();
   std::vector<State>* Successor(SearchType searchType,
-                                float (*heuristic)(State));
+                                double (*heuristic)(State));
 };
 
 class HitoriSolver {
@@ -34,16 +42,16 @@ class HitoriSolver {
   HitoriSolver(const char* filePath);
   ~HitoriSolver();
 
-  State GreedyBfs(State initialState, float (*heuristic)(State));
-  State AStar(State initialState, float (*heuristic)(State));
-  State HillClimbing(State initialState, float (*heuristic)(State));
-  State RandomizedHillClimbing(State initialState, float (*heuristic)(State));
-  State KStartHillClimbing(State initialState, float (*heuristic)(State));
+  State GreedyBfs(State initialState, double (*heuristic)(State));
+  State AStar(State initialState, double (*heuristic)(State));
+  State HillClimbing(State initialState, double (*heuristic)(State));
+  State RandomizedHillClimbing(State initialState, double (*heuristic)(State));
+  State KStartHillClimbing(State initialState, double (*heuristic)(State));
   State KStartRandomizedHillClimbing(State initialState,
-                                     float (*heuristic)(State));
+                                     double (*heuristic)(State));
   State SimulatedAnnealing(
       State initialState,
-      float (*heuristic)(State));  // @TODO: Get scheduler as a parameter
+      double (*heuristic)(State));  // @TODO: Get scheduler as a parameter
 
  private:
   int** _gameBoard;
