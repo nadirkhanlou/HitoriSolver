cd48f509a55d736d14c53f7575ef9a144d79b8b0
Mohammadmahdi Alijani <35420218+mma1377@users.noreply.github.com>
2019-12-18 11:43:53 +0330

    Some inputs added

--- HitoriCore/Source.cpp
+++ HitoriCore/Source.cpp
@@ -4,11 +4,11 @@
 double h(HitoriSolverCore::State) { return 0; }
 
 int main() {
-  HitoriSolverCore::HitoriSolver solver = HitoriSolverCore::HitoriSolver("input.txt");
+  HitoriSolverCore::HitoriSolver solver = HitoriSolverCore::HitoriSolver("18x18.txt");
 
   solver.PreProccess();
 
-  HitoriSolverCore::State result = solver.GreedyBfs(HitoriSolverCore::State(15), h);
+  HitoriSolverCore::State result = solver.GreedyBfs(HitoriSolverCore::State(18), h);
 
   solver.PrintState(result);
 
